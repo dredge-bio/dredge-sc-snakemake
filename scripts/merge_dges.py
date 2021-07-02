8a32dcec4e80fe3591df4d25b272044d9d203012
Patrick Golden <ptgolden0@gmail.com>
2021-07-02 13:24:35 -0400

    Output merged differential expressions in one large array
    ...rather than grouping them by transcript
    
    (also rename treatment -> transcript)

--- scripts/merge_dges.py
+++ scripts/merge_dges.py
@@ -2,18 +2,24 @@
 
 import json
 import sys
-from collections import defaultdict
 
 infile = sys.argv[1]
 with open(infile, 'r') as fp:
     data = json.load(fp)
 
-measurements_by_treatment = defaultdict(dict)
+parsed_measurements = []
 
 for cluster, measurements in data.items():
     for measurement in measurements:
-        treatment = measurement.pop('_row').replace('-', '|', 1)
+        transcript = measurement.pop('_row').replace('-', '|', 1)
 
-        measurements_by_treatment[treatment][cluster] = measurement
+        parsed_measurements.append({
+            "clusterID": cluster,
+            "transcriptID": transcript,
+            "logFC": measurement["avg_log2FC"],
+            "pctExpressedCluster": measurement["pct.1"],
+            "pctExpressedOther": measurement["pct.2"],
+            "pValue": measurement["p_val_adj"],
+        })
 
-print(json.dumps(measurements_by_treatment))
+print(json.dumps(parsed_measurements))
